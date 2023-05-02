faa6d08f6d799afde5465a8a4a97d235673d87bc
bcr33d <bcr33d@gmail.com>
2023-05-02 09:30:20 -0700

    added get and inc and streamlined code

--- src/main/java/edu/sjsu/cs249/kafkaTable/Main.java
+++ src/main/java/edu/sjsu/cs249/kafkaTable/Main.java
@@ -1,6 +1,7 @@
 package edu.sjsu.cs249.kafkaTable;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import io.grpc.ManagedChannelBuilder;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -17,15 +18,19 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.simple.SimpleLogger;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Clock;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 @Command
 public class Main {
@@ -33,16 +38,12 @@ public class Main {
         // quiet some kafka messages
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "warn");
     }
-    Properties properties = new Properties();
-
-    @Parameters(description = "the host:port of the kafka bootstrap server")
-    void setBootStrapServers(String server) {
-        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    }
 
     @Command
-    int publish(@Parameters(paramLabel = "topic-name") String name) throws IOException {
+    int publish(@Parameters(paramLabel = "kafkaHost:port") String server,
+                @Parameters(paramLabel = "topic-name") String name) throws IOException {
+        var properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         var producer = new KafkaProducer<>(properties, new StringSerializer(), new ByteArraySerializer());
         var br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0;; i++) {
@@ -58,13 +59,17 @@ public class Main {
     }
 
     @Command
-    int consume(@Parameters(paramLabel = "topic-name") String name,
+    int consume(@Parameters(paramLabel = "kafkaHost:port") String server,
+                @Parameters(paramLabel = "topic-name") String name,
                 @Parameters(paramLabel = "group-id") String id) throws InvalidProtocolBufferException {
+        var properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, id);
-       var consumer = new KafkaConsumer<>(properties, new StringDeserializer(), new ByteArrayDeserializer());
+        var consumer = new KafkaConsumer<>(properties, new StringDeserializer(), new ByteArrayDeserializer());
         consumer.subscribe(List.of(name));
         while (true) {
-            var records = consumer.poll(Duration.ofSeconds(20));
+            var records = consumer.poll(Duration.ofSeconds(1));
             for (var record: records) {
                 System.out.println(record.headers());
                 System.out.println(record.timestamp());
@@ -77,7 +82,9 @@ public class Main {
     }
 
     @Command
-    int listTopics() throws ExecutionException, InterruptedException {
+    int listTopics(@Parameters(paramLabel = "kafkaHost:port") String server) throws ExecutionException, InterruptedException {
+        var properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         try (var admin = Admin.create(properties)) {
             var rc = admin.listTopics();
             var listings = rc.listings().get();
@@ -89,10 +96,51 @@ public class Main {
     }
 
     @Command
-    int createTopic(@Parameters(paramLabel = "topic-name") String name) throws InterruptedException, ExecutionException {
+    int createTopic(@Parameters(paramLabel = "kafkaHost:port") String server,
+                    @Parameters(paramLabel = "topic-name") String name) throws InterruptedException, ExecutionException {
+        var properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, server);
         try (var admin = Admin.create(properties)) {
             var rc = admin.createTopics(List.of(new NewTopic(name, 1, (short) 1)));
             rc.all().get();
+        }
+        return 0;
+    }
+
+    @Command
+    int get(@Parameters(paramLabel = "key") String key,
+            @Parameters(paramLabel = "clientId") String id,
+            @Parameters(paramLabel = "grpcHost:port") String server) {
+        var clientXid = ClientXid.newBuilder().setClientid(id).setCounter((int)(System.currentTimeMillis()/1000)).build();
+        var stub = KafkaTableGrpc.newBlockingStub(ManagedChannelBuilder.forTarget(server).usePlaintext().build());
+        var rsp = stub.get(GetRequest.newBuilder().setKey(key).setXid(clientXid).build());
+        System.out.println(rsp.getValue());
+        return 0;
+    }
+
+        @Command
+    int inc(@Parameters(paramLabel = "key") String key,
+            @Parameters(paramLabel = "amount") int amount,
+            @Parameters(paramLabel = "clientId") String id,
+            @Option(names = "--repeat") boolean repeat,
+            @Option(names = "--concurrent") boolean concurrent,
+            @Parameters(paramLabel = "grpcHost:port", arity = "1..*") String[] servers) {
+        int count = repeat ? 2 : 1;
+        var clientXid = ClientXid.newBuilder().setClientid(id).setCounter((int)(System.currentTimeMillis()/1000)).build();
+        System.out.println(clientXid);
+        for (int i = 0; i < count; i++) {
+            var s = Arrays.stream(servers);
+            if (concurrent) s = s.parallel();
+            var result = s.map(server -> {
+                var stub = KafkaTableGrpc.newBlockingStub(ManagedChannelBuilder.forTarget(server).usePlaintext().build());
+                try {
+                    stub.inc(IncRequest.newBuilder().setKey(key).setIncValue(amount).setXid(clientXid).build());
+                    return server + ": success";
+                } catch (Exception e) {
+                    return server + ": " + e.getMessage();
+                }
+            }).collect(Collectors.joining(", "));
+            System.out.println(result);
         }
         return 0;
     }
